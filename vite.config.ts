6405950e440dc4e92d2c59278835ffda299d5a2b
Warno <anna.nikodimovna@yandex.ru>
2026-02-07 16:19:40 +0700

    chore: setup github pages via actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/weblarek/',
   css: {
     preprocessorOptions: {
       scss: {
-        loadPaths: [
-          './src/scss'
-        ],
+        loadPaths: ['./src/scss'],
       },
     },
   },
